cd1bfc37d589b141ea3e3fac4ea16235f8c667e1
Axel Goris <axelgoris99@gmail.com>
2021-09-25 13:30:29 +0200

    Spawn Enemy Functional + Class Sign

---
+++ Assets/Joint/Script/Signs.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signs : MonoBehaviour
+{
+    [SerializeField]
+    private int health;
+    [SerializeField]
+    private GameObject sign;
+    [SerializeField]
+    private int score;
+    [SerializeField]
+    private int probability;
+    public int Health
+    {
+        get { return health; }
+        set { SetHealth(value); }
+    }
+    
+    public void SetHealth(int value)
+    {
+        health = Mathf.Max(value, 0);
+        gameObject.SetActive(health > 1);  
+    }
+
+    public int Probability
+    {
+        get { return probability; }
+    }
+    public int Score
+    {
+        get { return score; }
+    }
+
+    public GameObject Sign
+    {
+        get { return sign; }
+    }
+
+}
--- Assets/Shooter/Script/spawnEnemy.cs
+++ Assets/Shooter/Script/spawnEnemy.cs
@@ -4,12 +4,49 @@ using UnityEngine;
 
 public class spawnEnemy : MonoBehaviour
 {
+    [SerializeField]
+    Signs[] formToSpawn;
+    [SerializeField]
+    Signs[] UniqueFormToSpawn;
+    List<Vector3> possibleSpawnPoint = new List<Vector3>();
+    List<int> probability = new List<int>();
     // Start is called before the first frame update
     void Start()
     {
+        int count = formToSpawn.Length;
         
+        for(int i = 0; i<count; i++)
+        {
+            for(int j = 0; j < formToSpawn[i].Probability; j++)
+            {
+                probability.Add(i);
+            }
+        }
+
+        for(int i = 0; i < 100; i++)
+        {
+            Vector3 spawnLeft = new Vector3(Random.Range(-15f, -14f), Random.Range(1f, 10f), 0);
+            Vector3 spawnRight = new Vector3(Random.Range(14f, 15f), Random.Range(1f, 10f), 0);
+            Vector3 spawnUp = new Vector3(Random.Range(-15f, 15f), Random.Range(10f, 11f), 0);
+            possibleSpawnPoint.Add(spawnLeft);
+            possibleSpawnPoint.Add(spawnRight);
+            possibleSpawnPoint.Add(spawnUp);
+        }
+
+        StartCoroutine(SpawnEnemy());
     }
 
+    IEnumerator SpawnEnemy()
+    {
+        int count = possibleSpawnPoint.Count;
+        while (true)
+        {
+            Vector3 spawnPoint = possibleSpawnPoint[Random.Range(0, count)];
+            GameObject toSpawn = formToSpawn[probability[Random.Range(0, probability.Count)]].Sign;
+            Instantiate(toSpawn, spawnPoint, new Quaternion(0,0,0,0));
+            yield return new WaitForSeconds(1f);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
