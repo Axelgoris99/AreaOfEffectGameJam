34b67fb7afc93de895dd202e85455eb5177090ea
Llleea <premont.lea@gmail.com>
2021-09-25 17:07:47 +0200

    button square

--- Assets/Puzzle/Scripts/SpawnShapes.cs
+++ Assets/Puzzle/Scripts/SpawnShapes.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnShapes : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class SpawnShapes : MonoBehaviour
     public List<Signs> collectedShapes = new List<Signs>();
     private Vector3 range;
     private Dictionary<string, int> inventaire = new Dictionary<string, int>();
+    [SerializeField]
+    public Dictionary<string, Button> buttons = new Dictionary<string, Button>();
+    private Signs formeButton;
 
     private void Awake()
     {
@@ -41,4 +45,25 @@ public class SpawnShapes : MonoBehaviour
         Gizmos.color = gizmosColor;
         Gizmos.DrawCube(transform.position, transform.localScale);
     }
+
+    public void SpawnShape(Signs forme)
+    {
+        formeButton = forme;
+        //spawn la forme
+        //
+        //
+        //décompte de l'inventaire
+        inventaire[forme.name] -= 1;
+        //update le compte texte sur le bouton
+        //Text text = buttons[forme.name].transform.GetComponentInChildren<Text>();
+        //text.text = "x " + inventaire[forme.name];
+
+        //gérer le cas où plus de formes de ce ype = désactiver bouton
+    }
+
+    public void UpdateText(Text text)
+    {
+        text.text = "x " + inventaire[formeButton.name];
+    }
+
 }
